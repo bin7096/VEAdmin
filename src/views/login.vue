222127a7178aeb3d9b2592304a8511626710ec01
bin7096 <bin_id@163.com>
2020-04-03 00:10:56 +0800

    20200403

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,44 @@
 <template class="box">
     <div class="login-box">
         <div class="login-form">
-            
+            <div class="login-head">
+                <span class="login-head-tab" v-for="(item, index) in tabs" :key="index" :class="index === tabIndex ? 'login-head-tab-show' : ''">{{item.context}}</span>
+            </div>
+            <div class="login-body">
+                <div class="login-body-tab" v-for="(item, index) in tabs" :key="index" :class="index === tabIndex ? 'login-body-tab-show' : ''">
+                    <input v-for="(val, key) in item.inputs" :key="key" :placeholder="val.prompt" :type="val.type"/>
+                </div>
+            </div>
+            <div class="login-foot"></div>
         </div>
     </div>
 </template>
 <style>
     @import '../assets/css/login.css';
 </style>
+<script>
+export default {
+    data() {
+        return {
+            tabIndex: 0,
+            tabs: [
+                {
+                    context: '登录',
+                    inputs: [
+                        {field: 'username', prompt: '账号/手机号/邮箱', reg: null, type: 'text'},
+                        {field: 'password', prompt: '请输入密码', reg: null, type: 'password'},
+                    ]
+                },
+                {
+                    context: '注册',
+                    inputs: [
+                        {field: 'telphone', prompt: '请输入手机号', reg: null, type: 'text'},
+                        {field: 'password', prompt: '请输入密码', reg: null, type: 'password'},
+                        {field: 'authcode', prompt: '请输入验证码', reg: null, type: 'text'},
+                    ]
+                },
+            ]
+        }
+    }
+}
+</script>
